4ddd818593780812d27d9ae44d2f09101195e2b1
jutaeho <dramavoy@daum.net>
2020-06-02 01:06:24 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# stDic
-행정표준용어사전
+# 행정표준용어 사전
+
+## **Install**
+
+```
+> npm install
+```
+
+## **dotenv**
+
+* env 파일에 연결할 PostgreSQL의 커넥션 정보를 입력
+
+```
+db_user=${user}
+db_host=${host}
+db_name=${name}
+db_pswd=${password}
+db_port=${port}
+```
+
+## **Get Started**
+
+```
+> npm start
+```
---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const query = require('./db/query');
+const mimeType = require('./config/mimeType.json');
+
+
+http.createServer((request, response) => {
+    const ext = path.parse(request.url).ext;
+    const publicPath = path.join(__dirname, 'src');
+    const fullPath = publicPath + request.url
+    const indexPath = `${publicPath}/index.html`;
+
+    if(Object.keys(mimeType).includes(ext)) {
+        readFile(response, `${fullPath}`, ext);
+    }
+    else {
+        if(request.url.indexOf('/search') > -1 ) {
+            query.set(request, response);
+        }
+        else {
+            readFile(response, indexPath, '.html');
+        }
+
+    }
+}).listen(4000);
+
+const readFile = (response, path, ext) => {
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            response.statusCode = 404;
+            response.setHeader('Content-Type', 'text/plain');
+            response.end('Page Not Found');
+        }
+        else {
+            response.statusCode = 200;
+            response.setHeader('Content-Type', mimeType[ext]);
+            response.end(data);
+        }
+    });
+}; 
